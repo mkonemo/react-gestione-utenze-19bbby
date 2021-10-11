dff8384e7b3f77713ee5182cead70f5a1469e86e
mkonemo <mkonemo@gmail.com>
2021-10-11 17:54:57 +0200

    2 files modified, 1 added

--- src/components/Expenses/AddExpense.js
+++ src/components/Expenses/AddExpense.js
@@ -2,26 +2,28 @@ import React from 'react';
 
 import Card from '../UI/Card';
 
+import classes from './AddExpense.module.css';
+
 const AddExpense = () => {
   const addExpenseHandler = (event) => {
     event.preventDefault();
   };
 
   return (
-<Card>
-    <form onSubmit={addExpenseHandler}>
-      <label htmlFor="date">Data</label>
-      <input id="date" type="date" />
-      <label htmlFor="amount">Importo (Euro)</label>
-      <input id="amount" type="number" />
-      <label htmlFor="description">Descrizione</label>
-      <input id="description" type="text" />
-      <label htmlFor="category">Categoria</label>
-      <input id="category" type="text" />
-      <label htmlFor="paid">Pagato</label>
-      <input id="paid" type="checkbox" />
-      <button type="submit">Aggiungi spesa</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addExpenseHandler}>
+        <label htmlFor="date">Data</label>
+        <input id="date" type="date" />
+        <label htmlFor="amount">Importo (Euro)</label>
+        <input id="amount" type="number" />
+        <label htmlFor="description">Descrizione</label>
+        <input id="description" type="text" />
+        <label htmlFor="category">Categoria</label>
+        <input id="category" type="text" />
+        <label htmlFor="paid">Pagato</label>
+        <input id="paid" type="checkbox" />
+        <button type="submit">Aggiungi spesa</button>
+      </form>
     </Card>
   );
 };
