eb555edf2a65d7e70073542b7c8f6f4242f23001
mkonemo <mkonemo@gmail.com>
2021-10-15 15:03:16 +0200

    1 file modified

--- src/components/Expenses/AddExpense.js
+++ src/components/Expenses/AddExpense.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
@@ -6,30 +6,75 @@ import Button from '../UI/Button';
 import classes from './AddExpense.module.css';
 
 const AddExpense = () => {
-
-const [enteredDate, setEnteredDate] = useState('');
-const [enteredAmount, setEnteredAmount] = useState('');
-const [enteredDescription, setEnteredDescription] = useState('');
-const [enteredCategory, setEnteredCategory] = useState('');
-const [checkedPaid, setCheckedPaid] = useState('false');
+  const [enteredDate, setEnteredDate] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDescription, setEnteredDescription] = useState('');
+  const [enteredCategory, setEnteredCategory] = useState('');
+  const [checkedPaid, setCheckedPaid] = useState('false');
 
   const addExpenseHandler = (event) => {
-    event.preventDefault();
+    event.preventDefault(enteredDate, enteredAmount, enteredDescription, enteredCategory, checkedPaid );
+    console.log()
   };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+  }
+
+  const descriptionChangeHandler = (event) => {
+    setEnteredDescription(event.target.value);
+  }
+
+  const categoryChangeHandler = (event) => {
+    setEnteredCategory(event.target.value);
+  } 
+
+  const paidCangeHandler = (event) => {
+    setCheckedPaid(event.target.value);
+  }
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addExpenseHandler}>
         <label htmlFor="date">Data</label>
-        <input id="date" type="date" />
+        <input
+          id="date"
+          type="date"
+          value={enteredDate}
+          onChange={dateChangeHandler}
+        />
         <label htmlFor="amount">Importo (Euro)</label>
-        <input id="amount" type="number" />
+        <input
+          id="amount"
+          type="number"
+          value={enteredAmount}
+          onChange={amountChangeHandler}
+        />
         <label htmlFor="description">Descrizione</label>
-        <input id="description" type="text" />
+        <input
+          id="description"
+          type="text"
+          value={enteredDescription}
+          onChange={descriptionChangeHandler}
+        />
         <label htmlFor="category">Categoria</label>
-        <input id="category" type="text" />
+        <input
+          id="category"
+          type="text"
+          value={enteredCategory}
+          onChange={categoryChangeHandler}
+        />
         <label htmlFor="paid">Pagato</label>
-        <input id="paid" type="checkbox" />
+        <input
+          id="paid"
+          type="checkbox"
+          value={checkedPaid}
+          onChange={paidCangeHandler}
+        />
         <Button type="submit">Aggiungi spesa</Button>
       </form>
     </Card>
