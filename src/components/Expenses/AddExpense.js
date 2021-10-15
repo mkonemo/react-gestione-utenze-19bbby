d160bdf5a6f5f5fa1872ff77f681a2f3991bc1f5
mkonemo <mkonemo@gmail.com>
2021-10-15 16:30:08 +0200

    1 file modified

--- src/components/Expenses/AddExpense.js
+++ src/components/Expenses/AddExpense.js
@@ -6,22 +6,21 @@ import Button from '../UI/Button';
 import classes from './AddExpense.module.css';
 
 const AddExpense = () => {
-  //const [enteredDate, setEnteredDate] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDescription, setEnteredDescription] = useState('');
- // const [enteredCategory, setEnteredCategory] = useState('');
- // const [checkedPaid, setCheckedPaid] = useState('false');
+  const [enteredCategory, setEnteredCategory] = useState('');
+  const [checkedPaid, setCheckedPaid] = useState('false');
 
   const addExpenseHandler = (event) => {
-    event.preventDefault(enteredDate, enteredAmount, enteredDescription, enteredCategory, checkedPaid );
-    console.log()
+    event.preventDefault( );
+    console.log(enteredDate, enteredAmount, enteredDescription, enteredCategory, checkedPaid)
   };
 
-  /*
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };
-*/
+
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
   }
@@ -29,7 +28,7 @@ const AddExpense = () => {
   const descriptionChangeHandler = (event) => {
     setEnteredDescription(event.target.value);
   }
-/*
+
   const categoryChangeHandler = (event) => {
     setEnteredCategory(event.target.value);
   } 
@@ -37,11 +36,17 @@ const AddExpense = () => {
   const paidCangeHandler = (event) => {
     setCheckedPaid(event.target.value);
   }
-*/
+
   return (
     <Card className={classes.input}>
       <form onSubmit={addExpenseHandler}>
-        
+        <label htmlFor="date">Data</label>
+        <input
+          id="date"
+          type="date"
+          value={enteredDate}
+          onChange={dateChangeHandler}
+        />
         <label htmlFor="amount">Importo (Euro)</label>
         <input
           id="amount"
@@ -56,7 +61,20 @@ const AddExpense = () => {
           value={enteredDescription}
           onChange={descriptionChangeHandler}
         />
-       
+        <label htmlFor="category">Categoria</label>
+        <input
+          id="category"
+          type="text"
+          value={enteredCategory}
+          onChange={categoryChangeHandler}
+        />
+        <label htmlFor="paid">Pagato</label>
+        <input
+          id="paid"
+          type="checkbox"
+          value={checkedPaid}
+          onChange={paidCangeHandler}
+        />
         <Button type="submit">Aggiungi spesa</Button>
       </form>
     </Card>
